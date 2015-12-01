567bfdd0bc66447baaaff008cd3b2dfdc04aaac6
deweytyl <deweytyl@grinnell.edu>
2015-12-01 06:06:49 +0000

    Add spec for creating new category

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -15,7 +15,7 @@ describe Admin::CategoriesController do
     get :index
     assert_response :redirect, :action => 'index'
   end
-
+  
   describe "test_edit" do
     before(:each) do
       get :edit, :id => Factory(:category).id
@@ -33,6 +33,26 @@ describe Admin::CategoriesController do
       assigns(:categories).should_not be_nil
     end
   end
+  
+  describe "test_new" do
+    before(:each) do
+      get :new
+    end
+    
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+    
+    it 'should have an empty category' do
+      assigns(:category).should_not be_nil
+      assigns(:category).display_name.should be_nil
+      assigns(:categories).should_not be_nil
+    end
+    
+  end
+    
 
   it "test_update" do
     post :edit, :id => Factory(:category).id
